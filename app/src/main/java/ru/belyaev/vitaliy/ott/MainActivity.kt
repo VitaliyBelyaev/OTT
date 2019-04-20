e4e1943cc218c145878f3edd2d2a6500066437e7
Vitaliy Belyaev <vitalik1392@mail.ru>
2019-04-20 11:03:27 +0300

    Initial commit

---
+++ app/src/main/java/ru/belyaev/vitaliy/ott/MainActivity.kt
@@ -0,0 +1,12 @@
+package ru.belyaev.vitaliy.ott
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
